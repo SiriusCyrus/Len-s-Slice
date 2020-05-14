23bf480aad8da106cc091f4aaf55495d68778875
Aniruddha Nag <cyrus.ani.2323@gmail.com>
2020-05-14 06:08:13 +0200

    Add files via upload

--- script.py
+++ script.py
@@ -1,19 +1,27 @@
+hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
 
-toppings = ['pepperoni', 'pineapple', 'cheese', 'sausage', 'olives', 'anchovies', 'mushrooms']
-prices = [2, 6, 1, 3, 2, 7, 2]
-num_pizzas = len(toppings)
-print ("We sell " + str(num_pizzas) + " different kinds of pizza!")
-pizzas = list(zip(prices,toppings))
-print(list(pizzas))
-pizzas.sort()
-print(list(pizzas))
-cheapest_pizza = pizzas[0]
-print("\n")
-print(cheapest_pizza)
-priciest_pizza = pizzas[-1]
-print(priciest_pizza)
-three_cheapest = pizzas[0:3]
-print(three_cheapest)
-print("\n")
-num_two_dollar_slices = prices.count(2)
-print(num_two_dollar_slices)
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+
+last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+total_price = 0
+for i in prices:
+  total_price = total_price + i
+
+average_price = total_price/len(prices)
+print("Average Haircut Price: ")
+print(average_price)
+new_prices = [reduce-5 for reduce in prices]
+print(new_prices)
+
+total_revenue = 0
+for i in range(len(hairstyles)):
+  total_revenue += prices[i]*last_week[i]
+
+print(total_revenue)
+average_daily_revenue = total_revenue/7
+print(average_daily_revenue)
+
+for i in range(len(new_prices)-1):
+  cuts_under_30 = [new_prices[i] for i in range(len(new_prices)-1) if prices[i] < 30]
+
+print(cuts_under_30)
